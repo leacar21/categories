6f210d9ff21bfd7cad003c2f55548406a223cb4e
Leandro Carrasco <carrascoleandro@gmail.com>
2017-08-19 17:46:18 -0300

    #UPDATE

--- src/main/java/com/despegar/aftersale/App.java
+++ src/main/java/com/despegar/aftersale/App.java
@@ -2,12 +2,10 @@ package com.despegar.aftersale;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
 import com.despegar.aftersale.dto.ReplieDTO;
-import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class App 
@@ -16,7 +14,6 @@ public class App
 
     public static void main(String[] args) {
 
-       CSVReader reader = null;
        try {
     	   BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("files/replies-original.csv"), "UTF-8"));
     	   List<ReplieDTO> beans = new CsvToBeanBuilder<ReplieDTO>(bufferedReader)
@@ -34,14 +31,6 @@ public class App
 
        } catch (Exception e) {
     	   e.printStackTrace();
-       } finally {
-          if (null != reader) {
-             try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-          } 
        }
     }
 }
